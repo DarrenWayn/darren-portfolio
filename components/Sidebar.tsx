c5e973449bce1d5ed35fbc00ad34374009e09de4
Darren Wayn <darrenwayn98@gmail.com>
2022-05-22 18:10:12 +0700

    Update Sidebar.tsx
    added clickable email

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -65,7 +65,7 @@ const Sidebar = () => {
 
       <button
         className="w-8/12 px-5 py-2 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-green to-blue-500 hover:scale-105 focus:outline-none"
-        onClick={() => window.open("darrenwayn98@@gmail.com")}
+        onClick={() => window.open("mailto:darrenwayn98@gmail.com")}
       >
         Email me
       </button>
