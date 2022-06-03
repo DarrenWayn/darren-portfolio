5b9a21402ef5086d5d35a45937b06ce658bedccb
Darren Wayn <darrenwayn98@gmail.com>
2022-06-03 14:08:18 +0700

    Update index.tsx
    added new <br /> style

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,20 @@ const About: NextPage = () => {
 
       <h6 className="my-3 text-base font-medium">
         Hi, My name is Darren and I’m a 23 years old Front-End Developer who loves to build stuff. My experience are range from using HTML & CSS to React/NextJS libraries like Material UI, Ant Design & TailwindCSS. 
-        <br />
+        <br 
+          style={{
+                    marginTop: '5px',
+                    marginBottom: '5px',
+                }}
+         />
         I also use API to fetch data into the site If you choose to work with me there will be a constant line of communication. 
         I believe that’s very important to deliver and and understand the project better.
-        <br />
+        <br 
+          style={{
+                    marginTop: '5px',
+                    marginBottom: '5px',
+                }}
+         />
         Let's talk! we'll better know if we're a good fit.
       </h6>
       <div
