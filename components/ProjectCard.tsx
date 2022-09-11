d07a0f28767150c6492b644f4fcde3732698a901
Darren Wayn <darrenwayn98@gmail.com>
2022-09-11 16:00:36 +0700

    added new feature

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -3,6 +3,7 @@ import { AiFillGithub, AiFillProject } from "react-icons/ai";
 import { MdClose } from "react-icons/md";
 import { IProject } from "../types";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 import Image from "next/image";
 
@@ -23,83 +24,99 @@ const ProjectCard: FunctionComponent<{
   const variants = {
     visible: { opacity: 1 },
     hidden: { opacity: 0 },
-  }
+  };
+
+  useEffect(() => {
+    const keyDownHandler = (e) => {
+      if (e.key === "Escape") {
+        e.preventDefault();
+        setShowDetail(false);
+      }
+    };
+
+    document.addEventListener("keydown", keyDownHandler);
+  }, []);
 
   return (
-    <div>
-      <Image
-        src={image_path}
-        alt={name}
-        className="cursor-pointer"
-        onClick={() => setShowDetail(true)}
-        layout="responsive"
-        height="150"
-        width="300"
-      />
-      {/* <img
+    <>
+      <div>
+        <Image
+          src={image_path}
+          alt={name}
+          className="cursor-pointer"
+          onClick={() => setShowDetail(true)}
+          layout="responsive"
+          height="150"
+          width="300"
+        />
+        {/* <img
         src={image_path}
         alt={name}
         className="cursor-pointer"
         onClick={() => setShowDetail(true)}
       /> */}
-      <p className="my-2 text-center">{name}</p>
+        <p className="my-2 text-center">{name}</p>
 
-      {showDetail && (
-        <motion.div
-          variants={variants}
-        >
-          <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
-            <div>
-              {/* <img src={image_path} alt={name} /> */}
-              <Image
-                src={image_path}
-                alt={name}
-                layout="responsive"
-                height="150"
-                width="300"
-              />
-              <div className="flex justify-center my-4 space-x-3">
-                <a
-                  href={github_url}
-                  className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
-                >
-                  <AiFillGithub /> <span>Github</span>
-                </a>
-                <a
-                  href={deployed_url}
-                  className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
-                >
-                  <AiFillProject /> <span>Project</span>
-                </a>
-              </div>
-            </div>
-
-            <div>
-              <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
-              <h3 className="mb-3 font-medium">{description}</h3>
-
-              <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
-                {key_techs.map((tech) => (
-                  <span
-                    key={tech}
-                    className="px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounde-sm"
+        {showDetail && (
+          <motion.div variants={variants}>
+            <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-dark-100">
+              <div>
+                {/* <img src={image_path} alt={name} /> */}
+                <Image
+                  src={image_path}
+                  alt={name}
+                  layout="responsive"
+                  height="150"
+                  width="300"
+                />
+                <div className="flex justify-center my-4 space-x-3">
+                  <a
+                    href={github_url}
+                    className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
                   >
-                    {tech}
-                  </span>
-                ))}
+                    <AiFillGithub /> <span>Github</span>
+                  </a>
+                  <a
+                    href={deployed_url}
+                    className="flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200"
+                  >
+                    <AiFillProject /> <span>Project</span>
+                  </a>
+                </div>
               </div>
-            </div>
 
-            <button
-              onClick={() => setShowDetail(false)}
-              className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-dark-200"
-            >
-              <MdClose size={30} />
-            </button>
-          </div>
-        </motion.div>
-      )}
-    </div>
+              <div>
+                <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
+                <h3 className="mb-3 font-medium">{description}</h3>
+
+                <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+                  {key_techs.map((tech) => (
+                    <span
+                      key={tech}
+                      className="px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounde-sm"
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <button
+                onClick={() => setShowDetail(false)}
+                className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-dark-200"
+              >
+                <MdClose size={30} />
+              </button>
+            </div>
+          </motion.div>
+        )}
+      </div>
+
+      <div
+        className={showDetail ? "overlay" : "overlay hidden"}
+        onClick={() => setShowDetail(false)}
+      ></div>
+    </>
   );
 };
 
